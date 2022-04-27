8a32e54e2f7ad06e1a9fd09e9e37d87b7141f14d
Artem Perminov <perminov.artem@eurostavka.ru>
2022-04-27 15:15:08 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const getExtension = (filePath) => path.extname(filePath).slice(1);
 const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf8'), getExtension(filepath));
 
-const genDiff = (path1, path2, format = 'stylish.txt') => {
+const genDiff = (path1, path2, format = 'stylish') => {
   const dataBefore = getData(buildFullPath(path1));
   const dataAfter = getData(buildFullPath(path2));
   const tree = buildDiffTree(dataBefore, dataAfter);
