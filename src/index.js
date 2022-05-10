b5ab04f8a014750f8aaec8d3c9b5fe726eefc98b
Artem Perminov <perminov.artem@eurostavka.ru>
2022-05-10 17:03:51 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import path from 'path';
 import fs from 'fs';
 import parse from './parsers.js';
 import buildDiffTree from './buildDiffTree.js';
-import makeFormattedOutput from './formatters/index.js';
+import formatter from './formatters/index.js';
 
 const getExtension = (filePath) => path.extname(filePath).slice(1);
 const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
@@ -12,7 +12,7 @@ const genDiff = (path1, path2, format = 'stylish') => {
   const dataBefore = getData(buildFullPath(path1));
   const dataAfter = getData(buildFullPath(path2));
   const tree = buildDiffTree(dataBefore, dataAfter);
-  return makeFormattedOutput(tree, format);
+  return formatter(tree, format);
 };
 
 export default genDiff;
